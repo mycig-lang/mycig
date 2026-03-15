e15f00dec3ed0f50e1d810a4d05d113413b62fb4
muedever <n-makoto0104@outlook.jp>
2026-03-15 21:13:52 +0900

    add Mycig Verification Script

--- Mycig/Program.fs
+++ Mycig/Program.fs
@@ -1,4 +1,7 @@
-﻿open Mycig.Compiler
+﻿open FParsec
+open Mycig.Compiler
+open Mycig.VerificationScript
+open Mycig
 
 [<EntryPoint>]
 let main _ =
@@ -18,4 +21,22 @@ func main() {
 "
     p.run code |> printfn "%i\n"
     p.getFlatAST() |> printfn "%A"
+    let code = """
+    states { Fresh, Used, Purged }
+
+rule Use { Fresh -> Used }
+
+infer MemoryError : Unit {
+    Use(Fresh, Used);
+    Purge(Used, Purged);
+}
+"""
+    let rf = ReadFile("mgvs")
+    let re = rf.Read("script/mycig.mgvs")
+    let code = if re.IsOk then re.ToString() else ""
+    match run VSParser.pProgram code with
+    | Success(res, _, _) ->
+        VSEngine.runAnalysis res
+    | Failure(err, _, _) ->
+        printfn "Parse Error:\n%s" err
     0
