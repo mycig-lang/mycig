1119221d251e7cb6e00a6815f0ef06ca3a44a201
muedever <n-makoto0104@outlook.jp>
2026-03-22 14:26:06 +0900

    [update]: TypeInference.fs

--- Mycig/compiler/TypeInference.fs
+++ Mycig/compiler/TypeInference.fs
@@ -1,16 +1,8 @@
 namespace Mycig.Compiler
 
 type TypeInference(fast: FlatAST) =
-    member val private FlatAST = fast.getAST() |> Array.indexed with get, set
-    member val private Structs = [||] with get, set
-    member val private Protocols = [||] with get, set
-    member val private Funcs = [||] with get, set
-    member val private FuncSTs = [||] with get, set
-    member val private FuncPRs = [||] with get, set
+    let flatAST = fast.getAST() |> Array.indexed
+    let mutable frames = [||]
 
     member this.init() =
-        this.Structs <- this.FlatAST |> Array.filter (fun (_, x) -> x.Type = "struct")
-        this.Protocols <- this.FlatAST |> Array.filter (fun (_, x) -> x.Type = "protocol")
-        this.Funcs <- this.FlatAST |> Array.filter (fun (_, x) -> x.Type = "func")
-        this.FuncSTs <- this.FlatAST |> Array.filter (fun (_, x) -> x.Type = "func_st")
-        this.FuncPRs <- this.FlatAST |> Array.filter (fun (_, x) -> x.Type = "func_pr")
+        frames <- flatAST |> Array.filter (fun (_, x) -> x.Type = "frame")
